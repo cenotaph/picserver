147a31066fa286c76295cb911258c64003e1418c
John W. Fail <john@pixelache.ac>
2021-04-08 19:32:36 +0300

    updated ecnosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,9 +13,9 @@ module.exports = {
       host : 'api.xn--mdaaa.ee',
       ref  : 'origin/main',
       repo : 'https://github.com/cenotaph/picserver',
-      path : '/var/www/õõõ/reno',
+      path : '/var/www/õõõ/reno/api',
       'pre-deploy-local': '',
-      'post-deploy' : 'ln -sf /var/www/õõõ/reno/shared/data /var/www/õõõ/reno/current/data && cp /var/www/õõõ/reno/shared/.passwd /var/www/õõõ/reno/current && npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'ln -sf /var/www/õõõ/reno/api/shared/data /var/www/õõõ/reno/api/current && cp /var/www/õõõ/reno/api/shared/.passwd /var/www/õõõ/reno/api/current && npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
